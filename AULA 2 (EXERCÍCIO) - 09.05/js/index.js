d60e46057713d185165de3202c9cf54b5881eb2a
Tamires Sousa <sousatamires8@gmail.com>
2023-05-16 19:24:40 -0300

    continuidade desafio 1

--- AULA 2 (EXERCÍCIO) - 09.05/js/index.js
+++ AULA 2 (EXERCÍCIO) - 09.05/js/index.js
@@ -3,40 +3,57 @@
    var jogadaUsuario = prompt ("O que voce deseja jogar? 1 = pedra, 2 = papel, 3 = tesoura")
    var jogadaUsuarioConvertida = Number(jogadaUsuario)
    var jogadaScript = Math.floor(Math.random() * 3) + 1
-   var vencedorPartida = 
+   var vezesUsuario = 0
+   var vezesScript = 0
 
    console.log('Jogada usuario: ' + jogadaUsuarioConvertida)
    console.log('Jogada script: ' + jogadaScript)
 
    if (jogadaUsuarioConvertida === jogadaScript) {
-     alert("Empatou")
+     alert("Rodada Empatda")
    } else {
 
      if(jogadaUsuarioConvertida === 1) {
-         if(jogadaScript === 2) {
-             alert('Voce perdeu')
+         if(jogadaScript === 2) { 
+          alert('Voce perdeu a rodada')
+          vezesScript++
          } else {
-             alert('Voce ganhou')
+            alert('Voce ganhou a rodada')
+          vezesUsuario++
          }
      }
    }
 
    if(jogadaUsuarioConvertida === 2) {
        if(jogadaScript === 1) {
-         alert('Voce ganhou')
+         alert('Voce ganhou a rodada')
+         vezesUsuario++
        } else {
-         alert('Voce perdeu')
+         alert('Voce perdeu a rodada')
+         vezesScript++
        }
    }
 
    if(jogadaUsuarioConvertida === 3) {
      if(jogadaScript === 2) {
-       alert('Voce ganhou')
+       alert('Voce ganhou a rodada')
+       vezesUsuario++
+
      } else {
-       alert('Voce perdeu')
+       alert('Voce perdeu a rodada')
+       vezesScript++
+
      }
    }
 
-   function vencedor(){
-    
+   function quantidadeVitoriasUsuario(vezesUsuario){
+      return(vezesUsuario);
    }
+
+   console.log(quantidadeVitoriasUsuario)
+
+   function quantidadeVitoriasScript(vezesScript){
+    return(vezesScript);
+ }
+
+    console.log(quantidadeVitoriasScript)
